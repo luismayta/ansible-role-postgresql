fdc734dfa7269826827424db601db69c815910e6
@slovacus <slovacus@gmail.com>
2020-05-18 06:43:24 -0500

    chore: implement actions keybase for make (#13)

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -2,13 +2,8 @@ test.help:
 	@echo '    Tests:'
 	@echo ''
 	@echo '        test                      show help'
-	@echo '        test run={module}         Run module test'
 	@echo '        test.all   	             Run all module test'
-	@echo '        test.picked               run test only commit'
 	@echo '        test.lint                 Run all pre-commit'
-	@echo '        test.lint.docker          Run all pre-commit in docker'
-	@echo '        test.syntax               Run all syntax in code'
-	@echo '        test.validate             Run all validation fixture dead in code'
 	@echo ''
 
 test:
@@ -16,27 +11,10 @@ test:
 	@if [ -z "${run}" ]; then \
 		make test.help;\
 	fi
-	@if [ -n "${run}" ]; then \
-		$(docker_test) run --rm $(DOCKER_SERVICE) bash -c "pipenv run py.test ${run}";\
-	fi
 
 test.all:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
-	$(docker_test) run --rm $(DOCKER_SERVICE) bash -c "pipenv run py.test"
-
-test.picked:
-	$(docker_test) run --rm $(DOCKER_SERVICE) bash -c "pipenv run py.test --picked"
-
-test.validate:
-	@echo $(MESSAGE) Running tests validation fixture $(END)
-	$(docker_test) run --rm $(DOCKER_SERVICE) bash -c "pipenv run py.test --dead-fixtures"
+	$(docker-test-run) bash -c "zunit --verbose"
 
 test.lint:
 	$(PIPENV_RUN) pre-commit run --all-files --verbose
-
-test.lint.docker:
-	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/dev.yml \
-		run --rm check sh -c "pipenv run pre-commit run --all-files --verbose"
-
-test.syntax:
-	@echo $(MESSAGE) Running tests $(END)
